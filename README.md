3be9cb0b74601527015ee6320d3d2f5667a2e50b
Hridoy Banik <hridoy.banik71@gmail.com>
2022-08-01 23:37:14 +0600

    add high level view of schema of object and their relationship

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# graphQL-design-principle" 
+## graphQL-design-principle 
+
+### Rule_One
+- Always start with  a high level view of the objects and their relationships before you deal with specific field
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server");
 
-exports.typeDefs = gql`
+const typeDefs = gql`
   type Query {
     cars: [Car!]!
   }
@@ -9,6 +9,33 @@ exports.typeDefs = gql`
     id: ID!
     color: String!
     make: String!
+  }
+
+  type ManualGroup{
+    id: ID!
+    name: String!
+    imageId: ID!
+    bodyHtml: String
+    memberships: [GroupMembership!]!
+  }
+  type AutomaticGroup{
+    id: ID!
+    name: String!
+    imageId: ID!
+    bodyHtml: String
+    memberships: [GroupMembership!]!
+    feature: [AutomaticGroupFeatures!]!
+    applyFeatureSeparately: Boolean!
+  }
+
+  type AutomaticGroupFeatures{
+    column: String!
+  }
+
+
+  type GroupMembership{
+    groupId: ID!
+    carId: ID!
   }
 `;
 
